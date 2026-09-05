5bd788701e2eee6b9f57d95f8883d33bd38068e4
Jaco Lombard <jaco.lombard@me.com>
2026-09-05 15:28:35 +0200

    feat(crm): export call plan timing services

--- src/phoenix_crm/services/__init__.py
+++ src/phoenix_crm/services/__init__.py
@@ -3,6 +3,7 @@
 from .activity_timeline import ActivityTimelineEntry, ActivityTimelineService
 from .activity_validation import ActivityIntegrityService, ActivityValidationResult
 from .call_cadence import CadenceResult, CallCadenceService
+from .call_plan_timing import CallPlanTiming, CallPlanTimingService, TimedCallPlanItem
 from .call_planning import CallPlanItem, CallPlanItemType, CallPlanningService
 from .follow_up_service import FollowUpService
 
@@ -16,5 +17,8 @@ __all__ = [
     "CallPlanItem",
     "CallPlanItemType",
     "CallPlanningService",
+    "CallPlanTiming",
+    "CallPlanTimingService",
     "FollowUpService",
+    "TimedCallPlanItem",
 ]
