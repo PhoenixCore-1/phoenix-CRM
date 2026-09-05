985fb039644067008acbb8ca751991b2a8a5b37b
Jaco Lombard <jaco.lombard@me.com>
2026-09-05 15:41:04 +0200

    feat(crm): export lead qualification service

--- src/phoenix_crm/services/__init__.py
+++ src/phoenix_crm/services/__init__.py
@@ -7,6 +7,7 @@ from .call_cadence import CadenceResult, CallCadenceService
 from .call_plan_timing import CallPlanTiming, CallPlanTimingService, TimedCallPlanItem
 from .call_planning import CallPlanItem, CallPlanItemType, CallPlanningService
 from .follow_up_service import FollowUpService
+from .lead_qualification import LeadQualificationResult, LeadQualificationService
 
 __all__ = [
     "ActivityIntegrityService",
@@ -24,5 +25,7 @@ __all__ = [
     "CRMIntelligenceService",
     "CRMIntelligenceType",
     "FollowUpService",
+    "LeadQualificationResult",
+    "LeadQualificationService",
     "TimedCallPlanItem",
 ]
