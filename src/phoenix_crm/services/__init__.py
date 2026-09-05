d5d7498ff27a472b25c12a62408b8ec2aafe54ed
Jaco Lombard <jaco.lombard@me.com>
2026-09-05 15:19:19 +0200

    feat(crm): export follow-up service

--- src/phoenix_crm/services/__init__.py
+++ src/phoenix_crm/services/__init__.py
@@ -2,10 +2,12 @@
 
 from .activity_timeline import ActivityTimelineEntry, ActivityTimelineService
 from .activity_validation import ActivityIntegrityService, ActivityValidationResult
+from .follow_up_service import FollowUpService
 
 __all__ = [
     "ActivityIntegrityService",
     "ActivityTimelineEntry",
     "ActivityTimelineService",
     "ActivityValidationResult",
+    "FollowUpService",
 ]
