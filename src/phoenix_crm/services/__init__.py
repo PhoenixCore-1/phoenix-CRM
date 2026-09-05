43914723d375af7ef363aec0dcf8c777c0326096
Jaco Lombard <jaco.lombard@me.com>
2026-09-05 15:13:22 +0200

    feat(crm): export activity integrity service

--- src/phoenix_crm/services/__init__.py
+++ src/phoenix_crm/services/__init__.py
@@ -1,5 +1,11 @@
 """Application services for Phoenix CRM 360."""
 
 from .activity_timeline import ActivityTimelineEntry, ActivityTimelineService
+from .activity_validation import ActivityIntegrityService, ActivityValidationResult
 
-__all__ = ["ActivityTimelineEntry", "ActivityTimelineService"]
+__all__ = [
+    "ActivityIntegrityService",
+    "ActivityTimelineEntry",
+    "ActivityTimelineService",
+    "ActivityValidationResult",
+]
